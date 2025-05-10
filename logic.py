b6db6883496d10ef5aa84dca7a29d615e94e5703
eli-nova-222 <eliasseranova@gmail.com>
2025-05-10 18:52:21 -0500

    logic setup

--- logic.py
+++ logic.py
@@ -42,8 +42,81 @@ class Logic(QMainWindow, Ui_MainWindow):
     def generate(self):
         try:
             num_assess = int(self.input_num_assess.text().strip())
+            self.label_notice.setText('')
+
+            if num_assess == 1:
+                self.label_assess1.show()
+                self.input_assess1.show()
+                self.label_assess2.hide()
+                self.input_assess2.hide()
+                self.label_assess3.hide()
+                self.input_assess3.hide()
+                self.label_assess4.hide()
+                self.input_assess4.hide()
+                self.label_assess5.hide()
+                self.input_assess5.hide()
+            elif num_assess == 2:
+                self.label_assess1.show()
+                self.input_assess1.show()
+                self.label_assess2.show()
+                self.input_assess2.show()
+                self.label_assess3.hide()
+                self.input_assess3.hide()
+                self.label_assess4.hide()
+                self.input_assess4.hide()
+                self.label_assess5.hide()
+                self.input_assess5.hide()
+            elif num_assess == 3:
+                self.label_assess1.show()
+                self.input_assess1.show()
+                self.label_assess2.show()
+                self.input_assess2.show()
+                self.label_assess3.show()
+                self.input_assess3.show()
+                self.label_assess4.hide()
+                self.input_assess4.hide()
+                self.label_assess5.hide()
+                self.input_assess5.hide()
+            elif num_assess == 4:
+                self.label_assess1.show()
+                self.input_assess1.show()
+                self.label_assess2.show()
+                self.input_assess2.show()
+                self.label_assess3.show()
+                self.input_assess3.show()
+                self.label_assess4.show()
+                self.input_assess4.show()
+                self.label_assess5.hide()
+                self.input_assess5.hide()
+            elif num_assess == 5:
+                self.label_assess1.show()
+                self.input_assess1.show()
+                self.label_assess2.show()
+                self.input_assess2.show()
+                self.label_assess3.show()
+                self.input_assess3.show()
+                self.label_assess4.show()
+                self.input_assess4.show()
+                self.label_assess5.show()
+                self.input_assess5.show()
+            else:
+                raise ValueError
 
 
         except ValueError:
             self.label_notice.setText(f'Must choose digit in range [1-5]')
             self.input_num_assess.setText('')
+            self.label_assess1.hide()
+            self.input_assess1.hide()
+
+            self.label_assess2.hide()
+            self.input_assess2.hide()
+
+            self.label_assess3.hide()
+            self.input_assess3.hide()
+
+            self.label_assess4.hide()
+            self.input_assess4.hide()
+
+            self.label_assess5.hide()
+            self.input_assess5.hide()
